b578db0cb033f7c03779a7616c7595ba581baceb
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2025-08-10 23:17:44 -0300

    atualização nas rotas

--- src/routes/Rotas.jsx
+++ src/routes/Rotas.jsx
@@ -5,7 +5,10 @@ import Presentation from '../components/pages/presentation/Presentation';
 import Register from '../components/pages/register/Register';
 import ForgotPassword from '../components/pages/forgot_password/ForgotPassword';
 import EmailSent from '../components/pages/email_sent/EmailSent';
-import Taks from '../components/pages/tasks/Tasks';
+import TaksList from '../components/pages/tasks/TaskList';
+import Dashboard from '../components/pages/dashboard/Dashboard';
+import Calendar from '../components/pages/calendar/Calendar';
+import Perfil from '../components/pages/perfil/Perfil';
 
 function Rotas() {
   return (
@@ -17,8 +20,10 @@ function Rotas() {
           <Route path='/register' element={<Register />} />
           <Route path='/forgot_password' element={<ForgotPassword />} />
           <Route path='/email_sent' element={<EmailSent />} />
-          <Route path='/tasks' element={<Taks />} />
-
+          <Route path='/tasks' element={<TaksList />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/calendar' element={<Calendar />} />
+          <Route path='/perfil' element={<Perfil />} />
         </Routes>
     </Router>
   );
