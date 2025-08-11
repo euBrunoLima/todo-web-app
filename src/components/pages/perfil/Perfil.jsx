cbbb3e2da57894117ff7e5467eea8cc85a7e4bbe
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2025-08-10 23:24:22 -0300

    nova pagina

---
+++ src/components/pages/perfil/Perfil.jsx
@@ -0,0 +1,13 @@
+import styles from './Perfil.module.css';
+
+function Perfil() {
+  return (
+    <div className={styles.perfil_container}>
+      <h1>Perfil</h1>
+      <p>Bem-vindo ao seu perfil!</p>
+      {/* Add more profile related content here */}
+    </div>
+  );
+}
+
+export default Perfil;
