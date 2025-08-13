baefebaab74d648dfb256e627c78b90b7e21a346
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2025-08-13 11:51:59 -0300

    atualização do componente

--- src/components/task_layout/categories/Categories.jsx
+++ src/components/task_layout/categories/Categories.jsx
@@ -1,9 +1,14 @@
-
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./CategoryTabs.module.css";
 
 function CategoryTabs({ categories, onSelect }) {
   const [active, setActive] = useState(categories[0]);
+
+  useEffect(() => {
+    if (categories && categories.length > 0) {
+      setActive(categories[0]);
+    }
+  }, [categories]);
 
   const handleClick = (category) => {
     setActive(category);
@@ -14,11 +19,11 @@ function CategoryTabs({ categories, onSelect }) {
     <div className={styles.category_tabs}>
       {categories.map((cat) => (
         <button
-          key={cat}
+          key={cat.id}
           className={`${styles.tab} ${active === cat ? styles.active : ""}`}
           onClick={() => handleClick(cat)}
         >
-          {cat}
+          {cat.name}
         </button>
       ))}
     </div>
