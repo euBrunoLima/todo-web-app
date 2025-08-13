ed6d4dbf2f0213b44e3358603a721738f65eb75d
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2025-08-13 11:46:30 -0300

    consumo api Categoria

---
+++ src/services/api/categoryServer.js
@@ -0,0 +1,28 @@
+
+export async function fetchUserCategories(token) {
+  try {
+    const response = await fetch('http://localhost:3000/api/categories/', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`, // token no header
+        'Content-Type': 'application/json'
+      }
+    });
+
+    let data;
+    try {
+      data = await response.json();
+    } catch {
+      data = null;
+    }
+
+    if (!response.ok) {
+      throw new Error(data?.mensagem || 'Erro ao buscar categorias');
+    }
+
+    return data;  // aqui terá o objeto com categorias pessoais, globais e todas_categorias
+
+  } catch (error) {
+    throw error;  // propaga o erro para o componente tratar
+  }
+}
