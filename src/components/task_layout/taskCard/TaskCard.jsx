9b677da6dbbd51669dc5eeadf23124b66162701f
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2025-08-18 11:28:12 -0300

    atualização no componente

--- src/components/task_layout/taskCard/TaskCard.jsx
+++ src/components/task_layout/taskCard/TaskCard.jsx
@@ -1,18 +1,51 @@
 import styles from './TaskCard.module.css';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function TaskCard({id, name, initialStatus, deadlineDate, deadlineTime, onStatusChange}) {
+function TaskCard({ id, name, initialStatus, deadlineDate, deadlineTime, onStatusChange }) {
     const [status, setStatus] = useState(initialStatus);
+    const navigate = useNavigate();
+
 
     const handleStatus = () => {
         const newStatus = !status;
         setStatus(newStatus);
-        if(onStatusChange) onStatusChange(id, newStatus);
+        if (onStatusChange) onStatusChange(id, newStatus);
     };
 
     useEffect(() => {
         console.log(status);
     }, [status]);
+
+    const formatTaskDate = (dateString) => {
+        if (!dateString) return '';
+
+        const date = new Date(dateString);
+        const today = new Date();
+        const tomorrow = new Date();
+        tomorrow.setDate(today.getDate() + 1);
+
+        const zeroTime = (d) => new Date(d.getFullYear(), d.getMonth(), d.getDate());
+
+        if (zeroTime(date).getTime() === zeroTime(today).getTime()) {
+            return 'Hoje';
+        } else if (zeroTime(date).getTime() === zeroTime(tomorrow).getTime()) {
+            return 'Amanhã';
+        } else {
+            return date.toLocaleDateString('pt-BR');
+        }
+    };
+
+    // Função para remover os segundos da hora
+    const formatTaskTime = (timeString) => {
+        if (!timeString) return '';
+        const [hours, minutes] = timeString.split(':');
+        return `${hours}:${minutes}`;
+    };
+
+    const navigateTo = (rota) => {
+        navigate(`/edit_task/${rota}`)
+    }
 
     return (
         <div className={styles.card_container}>
@@ -27,11 +60,11 @@ function TaskCard({id, name, initialStatus, deadlineDate, deadlineTime, onStatus
                 <label htmlFor={`check-${id}`} className={styles.check_label}></label>
             </div>
 
-            <div className={styles.texts}>
+            <div onClick={() => navigateTo(`${id}`)} className={styles.texts}>
                 <h3>{name ? name : ''}</h3>
                 <div className={styles.prazo}>
-                    <span>{deadlineDate ? deadlineDate : ''}</span>
-                    <span>{deadlineTime ? deadlineTime : ''}</span>
+                    <span>{formatTaskDate(deadlineDate)}</span>
+                    <span>{formatTaskTime(deadlineTime)}</span>
                 </div>
             </div>
         </div>
