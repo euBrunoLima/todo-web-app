fb5823d82e5d57308b03a283aadac2094766cb52
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2025-08-18 11:19:57 -0300

    atualização no input

--- src/components/forms/Input.jsx
+++ src/components/forms/Input.jsx
@@ -1,12 +1,12 @@
 import styles from './Input.module.css';
 
 
-function Input({type, text, name, placeholder, handleOnChange, value}){
+function Input({type, text, name, placeholder, handleOnChange, value, required = false}){
     return(
         <div className={styles.form_control}>
             <label className={styles.visually_hidden} htmlFor={name}>{text}:</label>
             <input
-                required 
+                required = {required}
                 type={type} 
                 name={name} 
                 id={name} 
