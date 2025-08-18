e1fd38bccabfd21b524c2c478dfea2a1498d5860
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2025-08-18 11:17:31 -0300

    atualização

--- src/services/api/taskService.js
+++ src/services/api/taskService.js
@@ -1,6 +1,8 @@
+const API_URL = 'http://localhost:3000/api/';
+
 export async function fetchUserTasks(token) {
   try {
-    const response = await fetch('http://localhost:3000/api/tasks/show', {
+    const response = await fetch(`${API_URL}tasks/show`, {
       method: 'GET',
       headers: {
         'Authorization': `Bearer ${token}`, // token no header
@@ -24,4 +26,58 @@ export async function fetchUserTasks(token) {
   } catch (error) {
     throw error;  // propaga o erro para ser tratado no componente
   }
+}
+export async function createTask(taskData, token) {
+  try {
+    const response = await fetch(`${API_URL}task`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${token}`, // token no header
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(taskData)
+    });
+
+    let data;
+    try {
+      data = await response.json();
+    } catch {
+      data = null;
+    }
+
+    if (!response.ok) {
+      throw new Error(data?.mensagem || 'Erro ao criar tarefa');
+    }
+
+    return data; // vai retornar o objeto com mensagem e resultado da criação
+
+  } catch (error) {
+    throw error; // propaga o erro para o componente tratar
+  }
+}
+
+export async function getTaskById(id, token) {
+  try {
+    const response = await fetch(`${API_URL}tasks/${id}/show`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`, // token no header
+        'Content-Type': 'application/json'
+      }
+    });
+
+    let data;
+    try {
+      data = await response.json();
+    } catch {
+      data = null;
+    }
+    if (!response.ok) {
+      throw new Error(data?.mensagem || 'Erro ao buscar tarefa');
+    }
+    return data; // vai retornar o objeto com mensagem e dados da tarefa
+  }catch (error) {
+    throw error; // propaga o erro para o componente tratar
+  }
+
 }
