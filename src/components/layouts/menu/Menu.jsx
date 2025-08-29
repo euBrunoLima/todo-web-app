850323392458165d825c4e3b92f7008b5ddf2bbd
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2025-08-29 11:57:06 -0300

    atualização do menu inferior

--- src/components/layouts/menu/Menu.jsx
+++ src/components/layouts/menu/Menu.jsx
@@ -4,12 +4,52 @@ import img1 from '../../../imgs/check.png';
 import img2 from '../../../imgs/calendar.png';
 import img4 from '../../../imgs/menu.png';
 import { NavLink } from 'react-router-dom';
+import SideBar from '../menu_lateral/SideBar.jsx';
+import { useState, useEffect, useRef } from 'react';
 
 function Menu(){
+
+    const [showMenu, setShowMenu] = useState(false);
+    const sideBarRef = useRef(null); // aqui cria o ref
+
+    
+    const toggleMenu = () =>{
+        setShowMenu(!showMenu)
+    }
+    // if(showMenu){
+    //     document.getElementsByTagName(body)
+    // }
+    
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (sideBarRef.current && !sideBarRef.current.contains(event.target)) {
+                setShowMenu(false);
+                console.log(showMenu)
+            }
+        };
+
+        if (showMenu) {
+            document.addEventListener("mousedown", handleClickOutside);
+            console.log(showMenu)
+
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+            console.log(showMenu)
+
+        };
+        
+  }, [showMenu]);
+
     return(
         <div className={styles.menu_container} >
+            {showMenu && <div className={styles.overlay}></div>}
+            <SideBar ref={sideBarRef} isActive={showMenu}/>
             <nav>
-                <img src={img4} alt="image menu" />
+                <NavLink onClick={toggleMenu}>
+                    <img src={img4} alt="image menu" />
+                </NavLink>
                 <NavLink to="/tasks" className={({ isActive }) => isActive ? styles.active : undefined}>
                     <img src={img1} alt="image task" />
                 </NavLink>
