2bf493aad27a5d25599d1ecc569118b770622281
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2025-09-10 14:28:01 -0300

    configs.  para usar o componente de calendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import Rotas from "./routes/Rotas";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { registerLicense } from "@syncfusion/ej2-base";
+
+import "@syncfusion/ej2-base/styles/material.css";
+import "@syncfusion/ej2-react-calendars/styles/material.css";
+import "./components/pages/calendar/Calendar.module.css"
+
+registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE);
 
 function App() {
   return (
