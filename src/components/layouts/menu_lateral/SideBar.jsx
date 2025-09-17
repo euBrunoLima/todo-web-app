10e304bc6958070d6bf436f822fea04bd4970bb3
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2025-09-17 14:47:28 -0300

    atualização no componente

--- src/components/layouts/menu_lateral/SideBar.jsx
+++ src/components/layouts/menu_lateral/SideBar.jsx
@@ -1,5 +1,5 @@
 import styles from './SideBar.module.css'
-import React, { forwardRef, useState, useContext } from 'react';
+import React, { forwardRef, useState, useContext, useEffect} from 'react';
 import banner from '../../../imgs/img_banner.png'
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../../context/AuthContext.jsx';
@@ -8,6 +8,7 @@ import { AuthContext } from '../../../context/AuthContext.jsx';
 import { FaUser, FaChartBar, FaCalendarAlt, FaListUl, FaPlusCircle } from "react-icons/fa";
 import CreateModal from './modal/CreateModal';
 import CategoryList from '../../task_layout/categoryList/CategotyList.jsx';
+import { fetchUserCategories } from "../../../services/api/categoryServer.js";
 
 const SideBar = forwardRef(({ isActive }, ref) => {
   const {token} = useContext(AuthContext);
@@ -27,15 +28,35 @@ const SideBar = forwardRef(({ isActive }, ref) => {
       setShowCategories(!showCategories)
   }
 
+  const [categorias, setCategorias] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const load = async () => {
+      try {
+        setLoading(true);
+        const data = await fetchUserCategories(token);
+        setCategorias(data.todas_categorias);
+      } catch (err) {
+        setError(err.message);
+      }finally {
+        setLoading(false);
+      }
+  };
+  
+  useEffect(() => {
+    load();
+  }, [token]);
+
+
   return (
     <>
-    {showModal && <CreateModal onClose={toggleModal} />}
+    {showModal && <CreateModal onClose={toggleModal} onCreate={load} />}
     <div ref={ref} className={`${styles.side_container} ${isActive ? styles.active : ''}`}>
       <div className={styles.img_box}>
         <img src={banner} alt="banner app" />
       </div>
 
-      {/* Menu */}
       <div className={styles.menu_container}>
         <ul>
           <li onClick={() => navigateTo("perfil")}>
@@ -54,12 +75,13 @@ const SideBar = forwardRef(({ isActive }, ref) => {
             <FaListUl className={styles.icon} /> Categorias
           </li>
           {showCategories && (
-            <CategoryList token={token} />
+            <CategoryList loading={loading} categorias={categorias} error={error} />
           )}
           <li onClick={() => toggleModal()}>
             <FaPlusCircle className={styles.icon} /> Criar nova
           </li>
         </ul>
+
       </div>
     </div>
     </>
